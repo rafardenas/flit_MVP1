52637c3143d28c8689f50b1f5b24482cc794ad9f
Rafael Cardenas <rafardenas@gmail.com>
2021-07-19 18:50:56 -0500

    deployment

--- web_app/config2.py
+++ web_app/config2.py
@@ -25,8 +25,7 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 25
-
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or "http://localhost:9200"
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
 
