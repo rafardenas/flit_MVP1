a2a937e283d86db150fc17fb078c3950ed0cb114
Rafael Cardenas <rafardenas@gmail.com>
2021-07-19 14:35:47 -0500

    deploy changes

--- web_app/config2.py
+++ web_app/config2.py
@@ -10,7 +10,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     print(SQLALCHEMY_DATABASE_URI)
     #print(os.environ.get('DATABASE_URL'))
