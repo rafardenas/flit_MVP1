7b28e22d21bfcaeab20ff28d8f955f8eea750c40
Rafael Cardenas <rafardenas@gmail.com>
2021-08-23 10:26:39 -0500

    migrate db

--- web_app/app/__init__.py
+++ web_app/app/__init__.py
@@ -44,7 +44,7 @@ def create_app(config_class=Config):
     app.register_blueprint(user_bp, url_prefix="/user")
     #app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) if app.config['ELASTICSEARCH_URL'] else None
     #DATABASE_URL = os.environ['DATABASE_URL']
-    #psycopg2.connect(database='encuentracargasmx_db', user='rafardenas@encuentracargasmx', host='encuentracargasmx.postgres.database.azure.com', password='1Rafiqui#', port='5432', sslmode='require')
+    #psycopg2.connect(database='db', user='user', host='host', password='pwwd', port='port', sslmode='require')
     #psycopg2.connect(C, sslmode='require')
 
     
